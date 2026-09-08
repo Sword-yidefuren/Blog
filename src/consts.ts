1b15996bd4e5f1089bce920d070b48f4c38581b3
Sword-yidefuren <2281567400@qq.com>
2026-09-08 20:20:35 +0800

    feat: 站点标题改为我的小站，并修正站点地址

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '我的小站';
+export const SITE_DESCRIPTION = 'Sword 的个人博客，记录学习与生活';
